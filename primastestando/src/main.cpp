3ab4811ffd66eb3e64a248ed10427946cee78ba4
ludmagz <lclm@cin.ufpe.br>
2025-10-01 18:55:17 -0300

    Print "Hello World" no ILI9341

---
+++ primastestando/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+// Defina os pinos do Raspberry Pi Pico para o display
+#define TFT_CS    5  // Chip Select
+#define TFT_RST   17  // Reset
+#define TFT_DC    16
+#define TFT_MOSI  11
+#define TFT_SCK   2
+  // Data/Command
+
+// Crie um objeto Adafruit_ILI9341
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
+
+void setup() {
+  Serial.begin(115200);
+
+  // Inicializa o display
+  tft.begin();
+  tft.setRotation(3);  // Ajuste a rotação do display, dependendo da sua preferência
+  tft.fillScreen(ILI9341_BLACK);  // Preenche o fundo com a cor preta
+
+  // Defina as propriedades de texto
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(50, 50);
+  tft.println("Hello, World!");  // Texto de teste
+}
+
+void loop() {
+  // Você pode adicionar código adicional aqui
+}
